b5b54a4fb0172dac0d4dba53e76d179f0fdf9d3b
ao0727 <qingduj@gmail.com>
2023-11-07 09:30:04 +0900

    プロジェクト ファイルを追加します。

---
+++ kadai5.1/kadai5.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int countDivisors(int n) {
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int N;
+    cout << "Nを入力: ";
+    cin >> N;
+
+    int count = 0;
+    for (int i = 1; i <= N; i += 2) {
+        if (countDivisors(i) == 8) {
+            count++;
+        }
+    }
+
+    cout << "約数が8個の奇数の数: " << count << endl;
+
+    
+}
